cdcf29fe8de3b5b71a328f708e11ad3e23654173
saurabhkkanaujia <saurabhkumarkanaujia@cedcoss.com>
2022-03-24 12:23:37 +0530

    Corrected Dashboard Redirection

--- src/app/controllers/PanelController.php
+++ src/app/controllers/PanelController.php
@@ -9,7 +9,10 @@ class PanelController extends Controller{
     }
 
     public function dashboardAction(){
-        if ($this->session->loginUser == null) {
+        // // if ($this->session->loginUser == null) {
+        //     print_r(isset(json_decode($this->cookies->get('remember-me')->getValue())->email));
+        //     die();
+        if (json_decode($this->cookies->get('remember-me')->getValue())->email == null) {
             $this->response->redirect('user/signin');
         } else {
             $this->view->time = $this->time;
