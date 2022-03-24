1d9f2bd0a19734f6b47b88282ede18fe41279213
saurabhkkanaujia <saurabhkumarkanaujia@cedcoss.com>
2022-03-24 12:38:03 +0530

    Corrected Dashboard Redirection through cookie

--- src/app/controllers/PanelController.php
+++ src/app/controllers/PanelController.php
@@ -9,13 +9,10 @@ class PanelController extends Controller{
     }
 
     public function dashboardAction(){
-        // // if ($this->session->loginUser == null) {
-        //     print_r(isset(json_decode($this->cookies->get('remember-me')->getValue())->email));
-        //     die();
-        if (json_decode($this->cookies->get('remember-me')->getValue())->email == null) {
-            $this->response->redirect('user/signin');
-        } else {
+        if (json_decode($this->cookies->get('remember-me')->getValue())->email != null || $this->session->get('loginUser') !=null) {
             $this->view->time = $this->time;
+        } else {
+            $this->response->redirect('user/signin');
         }
     }
     
